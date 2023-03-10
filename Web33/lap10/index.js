2ec0dc5b2f415a3fcf14b39cd54c3fc63e487921
hoanghieu2001 <104782203+hoanghieu2001@users.noreply.github.com>
2023-03-10 19:25:21 +0700

    day

---
+++ Web33/lap10/index.js
@@ -0,0 +1,27 @@
+// 1 + null = 1
+// 1 + undefined = NaN
+
+// Gán kết hợp với toán tử số học
+// x +=1 ;//x = x+1 ->2
+// x/ =2;// y/2->3
+//  let a =1;
+//  console.log(a++ + a++);
+//  console.log(a);
+//  let b =1;
+//  console.log(++b+ ++b);
+//  console.log(b);
+
+
+
+
+// let a =1, b=(a%2)*2,c=a++ - b--,d="0";
+// console.log(a+b+c+d);
+// console.log(a-b+c-d);
+// console.log(a-- +(b-- *c)/d);
+// // console.log(++a - +b *c )
+
+// a=1 
+// b=1
+// c=0 +2
+// d="-0"
+
